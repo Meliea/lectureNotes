8cf514f2254d2543383415f37da319082a69632a
Meliea Taylor <melieaftaylor@gmail.com>
2020-11-15 21:39:08 -0500

    initial push

---
+++ 09-switch.js
@@ -0,0 +1,29 @@
+/*
+Switch Statements
+*/
+
+let friend = 'Tom';
+
+switch (friend){
+    case 'Tom':
+        console.log('Hey Tom, when will you bring sourdough bread for me');
+        break;
+    case 'Kenn':
+        console.log('Hey Kenn, do you own a wolf in your backyard');
+        break;
+    case 'Alecx':
+        console.log(' Hey Alecx, how many cards do you own');
+        break;
+    default: 
+        console.log(`I'm sorry, ${friend}, but do I know you?`);
+}
+
+let yep = true;
+
+switch(typeof yep == 'string' || typeof yep == 'boolean'){
+    case true:
+        console.log('yep is a string or a boolean');
+        break;
+    default:
+        console.log('yep is not a string or a number');
+}
